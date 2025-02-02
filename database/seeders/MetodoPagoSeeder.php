723f478191a231341f0ae8d42c0b6d6471ebcde4
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-02-02 12:16:06 -0400

    seeder de metodos de pago

--- database/seeders/MetodoPagoSeeder.php
+++ database/seeders/MetodoPagoSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Carbon;
+use App\Models\MetodoPago;
 
 class MetodoPagoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Deshabilita temporalmente las restricciones de clave foránea
+        Schema::disableForeignKeyConstraints();
+        MetodoPago::truncate(); // Limpia la tabla antes de insertar
+        Schema::enableForeignKeyConstraints();
+
+        // Inserta los métodos de pago
+        MetodoPago::insert([
+            ['nombre' => 'Efectivo', 'created_at' => now(), 'updated_at' => now()],
+            ['nombre' => 'Qr', 'created_at' => now(), 'updated_at' => now()],
+            ['nombre' => 'Tarjeta de Débito', 'created_at' => now(), 'updated_at' => now()],
+            ['nombre' => 'Tarjeta de Credito', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 }
